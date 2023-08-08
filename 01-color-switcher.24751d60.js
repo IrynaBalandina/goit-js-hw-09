009d82c8af56eda603b1137243e30050a010e60c
IrynaBalandina <128047133+IrynaBalandina@users.noreply.github.com>
2023-08-08 18:55:22 +0000

    Deploying to gh-pages from @ IrynaBalandina/goit-js-hw-09@a0fa8324094af2f54fa6a632331e97f8d3085fa8 🚀

---
+++ 01-color-switcher.24751d60.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(function(a){t.disabled=!0,e.disabled=!1,d=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(function(a){e.disabled=!0,t.disabled=!1,clearInterval(d)})),e.disabled=!0;let d=null;
+//# sourceMappingURL=01-color-switcher.24751d60.js.map
---
+++ 01-color-switcher.b1b79526.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(function(n){t.disabled=!0,e.disabled=!1,a=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),e.addEventListener("click",(function(n){e.disabled=!0,t.disabled=!1,clearInterval(a)})),e.disabled=!0;var a=null}();
+//# sourceMappingURL=01-color-switcher.b1b79526.js.map
